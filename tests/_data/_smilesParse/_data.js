d5f0c237b8dae244f387664ebc9841469af1c70b
OlgaRedozubova <olgared82@gmail.com>
2021-01-13 10:21:24 +0200

    Added tests for include_smiles

---
+++ tests/_data/_smilesParse/_data.js
@@ -0,0 +1,16 @@
+module.exports = [
+  'CC[C@H](C)[C@H](NC(=O)[C@H](CCC(O)=O)NC(=O)[C@H](CCC(O)=O)NC(=O)[C@H](CC1=CC=CC=C1)NC(=O)[C@H](CC(O)=O)NC(=O)CNC(=O)[C@H](CC(N)=O)NC(=O)CNC(=O)CNC(=O)CNC(=O)CNC(=O)[C@@H]1CCCN1C(=O)[C@H](CCCNC(N)=N)NC(=O)[C@@H]1CCCN1C(=O)[C@H](N)CC1=CC=CC=C1)C(=O)N1CCC[C@H]1C(=O)N[C@@H](CCC(O)=O)C(=O)N[C@@H](CCC(O)=O)C(=O)N[C@@H](CC1=CC=C(O)C=C1)C(=O)N[C@@H](CC(C)C)C(O)=O',
+  'CC(C)C[C@H](NC(=O)[C@@H](COC(C)(C)C)NC(=O)[C@H](CC1=CC=C(O)C=C1)NC(=O)[C@H](CO)NC(=O)[C@H](CC1=CNC2=CC=CC=C12)NC(=O)[C@H](CC1=CN=CN1)NC(=O)[C@@H]1CCC(=O)N1)C(=O)N[C@@H](CCCN=C(N)N)C(=O)N1CCC[C@H]1C(=O)NNC(N)=O',
+  'NC(=O)CC[C@@H]1NC(=O)[C@H](CC2=CC=CC=C2)NC(=O)[C@H](CC2=CC=C(O)C=C2)NC(=O)CCSSC[C@H](NC(=O)[C@H](CC(N)=O)NC1=O)C(=O)N1CCC[C@H]1C(=O)N[C@@H](CCCNC(N)=N)C(=O)NCC(N)=O',
+  'CC(C)C[C@H](NC(=O)[C@@H](CCCNC(N)=O)NC(=O)[C@H](CC1=CC=C(O)C=C1)NC(=O)[C@H](CO)NC(=O)[C@@H](CC1=CN=CC=C1)NC(=O)[C@@H](CC1=CC=C(Cl)C=C1)NC(=O)[C@@H](CC1=CC2=CC=CC=C2C=C1)NC(C)=O)C(=O)N[C@@H](CCCNC(N)=N)C(=O)N1CCC[C@H]1C(=O)N[C@H](C)C(N)=O',
+  'CC[C@@H]1NC(=O)[C@H]([C@H](O)[C@H](C)C\C=C\C)N(C)C(=O)[C@H](C(C)C)N(C)C(=O)[C@H](CC(C)C)N(C)C(=O)[C@H](CC(C)C)N(C)C(=O)[C@@H](C)NC(=O)[C@H](C)NC(=O)[C@H](CC(C)C)N(C)C(=O)[C@@H](NC(=O)[C@H](CC(C)C)N(C)C(=O)CN(C)C1=O)C(C)C',
+  'NCCCC[C@H](NC(=O)[C@@H]1CCCN1C(=O)[C@@H]1CSSC[C@H](N)C(=O)N[C@@H](CC2=CC=CC=C2)C(=O)N[C@@H](CC2=CC=CC=C2)C(=O)N[C@@H](CCC(N)=O)C(=O)N[C@@H](CC(N)=O)C(=O)N1)C(=O)NCC(N)=O',
+  '[H][C@]1(NC(=O)[C@H](CCCCN)NC(=O)[C@@H](CC2=CNC3=C2C=CC=C3)NC(=O)[C@H](CC2=CC=CC=C2)NC(=O)[C@H](CSSC[C@H](NC1=O)C(=O)N[C@H](CO)[C@@H](C)O)NC(=O)[C@H](N)CC1=CC=CC=C1)[C@@H](C)O',
+  'CC1=NC=C(COP(O)(O)=O)C(C=O)=C1O',
+  'OC[C@H]1O[C@@H]([C@H](O)[C@@H]1OP(O)(=O)O[C@]([H])(C)CNC(=O)CC[C@]1(C)[C@@H](CC(=O)N)[C@@]2([H])N([Co]C#N)\C1=C(C)/C1=N/C(=C\C3=N\C(=C(C)/C4=N[C@]2(C)[C@@](C)(CC(=O)N)[C@@H]4CCC(=O)N)\[C@@](C)(CC(=O)N)[C@@H]3CCC(=O)N)/C(C)(C)[C@@H]1CCC(=O)N)N1C=NC2=CC(C)=C(C)C=C12',
+  'NC1=NC(=O)C2=C(NCC(CNC3=CC=C(C=C3)C(=O)N[C@@H](CCC(O)=O)C(O)=O)N2)N1',
+  'N[C@@H](CC1=CN=CN1)C(O)=O',
+  'C[S+](CC[C@H](N)C(O)=O)C[C@H]1O[C@H]([C@H](O)[C@@H]1O)N1C=NC2=C(N)N=CN=C12',
+  'CC(=O)C(O)=O',
+  'N[C@@H](CC1=CC=CC=C1)C(O)=O'
+];
---
+++ tests/_smilesParse.js
@@ -0,0 +1,106 @@
+let chai = require('chai');
+let should = chai.should();
+const notIncludeSymbols = require('./_ascii');
+
+let MM = require('../lib/mathpix-markdown-model/index').MathpixMarkdownModel;
+
+const Window = require('window');
+const window = new Window();
+global.window = window;
+global.document = window.document;
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+global.DOMParser = new JSDOM().window.DOMParser;
+
+
+describe('Check parse Smiles inline:', () => {
+  const tests = require('./_data/_smilesParse/_data');
+  const options = {
+    cwidth: 800,
+    outMath: {
+      include_smiles: true,
+      include_svg: false,
+    }};
+  tests.forEach(function(item, index) {
+    const smilesInline = `<smiles>${item}</smiles>`;
+
+    const html = MM.render(smilesInline, options);
+    const data = MM.parseMarkdownByHTML(html, false);
+    describe((index + 1) + '. [input_smiles] => ' + item, () => {
+      it('Should be length = 1', (done) => {
+        data.should.have.length(1);
+        done();
+      });
+      it('Should be return smiles =>', function(done) {
+        data[0].should.have.property('type', 'smiles');
+        data[0].should.have.property('value', item);
+        done();
+      });
+    });
+  });
+});
+
+describe('Check parse Smiles block:', () => {
+  const tests = require('./_data/_smilesParse/_data');
+  const options = {
+    cwidth: 800,
+    outMath: {
+      include_smiles: true,
+      include_svg: false,
+    }};
+  tests.forEach(function(item, index) {
+    let smilesInline = '```smiles';
+    smilesInline += '\n';
+    smilesInline += item;
+    smilesInline += '\n';
+    smilesInline += '```';
+
+    const html = MM.render(smilesInline, options);
+    const data = MM.parseMarkdownByHTML(html, false);
+    describe((index + 1) + '. [input_smiles] => ' + item, () => {
+      it('Should be length = 1', (done) => {
+        data.should.have.length(1);
+        done();
+      });
+      it('Should be return smiles =>', function(done) {
+        data[0].should.have.property('type', 'smiles');
+        data[0].should.have.property('value', item);
+        done();
+      });
+    });
+  });
+});
+
+describe('Check parse Smiles with svg:', () => {
+  const tests = require('./_data/_smilesParse/_data');
+  const options = {
+    cwidth: 800,
+    outMath: {
+      include_smiles: true
+    }};
+  tests.forEach(function(item) {
+    const smilesInline = `<smiles>${item}</smiles>`;
+
+    const html = MM.render(smilesInline, options);
+    const data = MM.parseMarkdownByHTML(html, false);
+    describe('input_smiles => ' + item, () => {
+      it('Should be length = 2', (done) => {
+        data.should.have.length(2);
+        done();
+      });
+      it('Should be return smiles =>', function(done) {
+        data[0].should.have.property('type', 'smiles');
+        data[0].should.have.property('value', item);
+        done();
+      });
+
+      it('Should be return svg =>', function(done) {
+        data[1].should.have.property('type', 'svg');
+        const doc = JSDOM.fragment(data[1].value);
+        doc.firstChild.tagName.should.equal('svg');
+        done();
+      });
+    });
+  });
+});
