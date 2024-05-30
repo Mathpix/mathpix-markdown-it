0181f0aa2228dd04466cfda7cfe1266939ece9a5
Olga Redozubova <olgaredozubova@Olgas-MacBook-Pro.local>
2024-05-30 17:10:34 +0300

    Added tests

--- tests/_sepatation_span.js
+++ tests/_sepatation_span.js
@@ -5,6 +5,7 @@ let MM = require('../lib/mathpix-markdown-model/index').MathpixMarkdownModel;
 
 
 const { JSDOM } = require("jsdom");
+const tests = require("./_data/_sepatation_span/_data");
 const jsdom = new JSDOM();
 global.window = jsdom.window;
 global.document = jsdom.window.document;
@@ -20,6 +21,24 @@ describe('Check SanitizeHtml for markdownToHTML. [disallowedTagsMode = recursive
     const html = MM.markdownToHTML(test.mmd, options);
     
     describe('Should be <span></span> ' + test.describe, () => {
+      it('Checking result html', (done) => {
+        html.trim().should.equal(test.html);
+        done();
+      });
+    });
+  })
+});
+
+describe('Check separation span for code block', () => {
+  const options = {
+    htmlTags: true,
+    previewUuid: "98790064-1a09-406b-8311-efb72e541d01"
+  };
+  const tests = require('./_data/_sepatation_span/_data_fence');
+
+  tests.forEach(function(test){
+    const html = MM.markdownToHTML(test.mmd, options);
+    describe('Should be ' + test.describe, () => {
       it('Checking result html', (done) => {
         html.trim().should.equal(test.html);
         done();
